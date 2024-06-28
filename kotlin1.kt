93159fb9524a69c07670e8bf358914963d7286bf
Tu Nombre <tuemail@ejemplo.com>
2024-06-28 11:46:51 -0500

    Clases

--- kotlin1.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    var monthsoftheyear = arrayOf("enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre")
-    for ((posicion, mes) in monthsoftheyear.withIndex()){
-        println("la posición $posicion contiene el mes de $mes ")
-    }
-    
-}
